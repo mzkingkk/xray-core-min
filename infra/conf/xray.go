17f73dc6fff470b31f0c71f90f303e62fb1ed43a
mzkingkk <978964112@qq.com>
2024-07-21 13:33:19 +0800

    delete freedom

--- infra/conf/xray.go
+++ infra/conf/xray.go
@@ -27,7 +27,6 @@ var (
 	}, "protocol", "settings")
 
 	outboundConfigLoader = NewJSONConfigLoader(ConfigCreatorCache{
-		"freedom":     func() interface{} { return new(FreedomConfig) },
 		"http":        func() interface{} { return new(HTTPClientConfig) },
 		"vless":       func() interface{} { return new(VLessOutboundConfig) },
 	}, "protocol", "settings")
--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -31,7 +31,6 @@ import (
 
 	// Inbound and outbound proxies.
 	_ "github.com/xtls/xray-core/proxy/dokodemo"
-	_ "github.com/xtls/xray-core/proxy/freedom"
 	_ "github.com/xtls/xray-core/proxy/http"
 	_ "github.com/xtls/xray-core/proxy/vless/inbound"
 	_ "github.com/xtls/xray-core/proxy/vless/outbound"
